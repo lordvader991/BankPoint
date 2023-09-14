ffdb06295aa6d6d4058be1e15eadafe8b972d7aa
Administrador <josemariazapata12@gmail.com>
2023-09-13 23:27:18 -0400

    Versión 1.2.1

--- app/src/main/java/com/example/prueba/Entidadesfinancieras.java
+++ app/src/main/java/com/example/prueba/Entidadesfinancieras.java
@@ -27,11 +27,50 @@ public class Entidadesfinancieras extends AppCompatActivity {
     }
     public void mostrarMapaLugar1 (View view){ //banco mercantil
         List<Ubicacion> ubicacionesLugar1 = new ArrayList<>();
-        ubicacionesLugar1.add(new Ubicacion(-17.728719,-63.166313, "Hipermaxi"));
+        ubicacionesLugar1.add(new Ubicacion(-17.728719,-63.166313, "Hipermaxi Supercenter"));
         ubicacionesLugar1.add(new Ubicacion(-17.7557677,-63.1722757, "Domingo Savio"));
         ubicacionesLugar1.add(new Ubicacion(-17.7539786,-63.1996628, "Ventura Mall"));
-        ubicacionesLugar1.add(new Ubicacion(-17.767475, -63.143535, "Parque Industrial"));
-        ubicacionesLugar1.add(new Ubicacion(-17.781183, -63.189088,"Av Cañoto"));
+        ubicacionesLugar1.add(new Ubicacion(-17.767475, -63.143535, "Distrito industrial por Av trasvesal 1"));
+        ubicacionesLugar1.add(new Ubicacion(-17.781187,-63.3332507,"Av Cañoto"));
+        ubicacionesLugar1.add(new Ubicacion(-17.8102584,-63.2082709,"Hipermaxi las palmas"));
+        ubicacionesLugar1.add(new Ubicacion(-17.795368,-63.3048106,"Hipermaxi roca y coronado"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7748637,-63.1741118,"Mercado Chacarrilla"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7778833,-63.2093115,"cerca al monumento Cristo Redentor"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7824545,-63.1841092,"av Melchor Pinto"));
+        ubicacionesLugar1.add(new Ubicacion(-17.8096168,-63.2135569,"4to anillo doble vía la guardia"));
+        ubicacionesLugar1.add(new Ubicacion(-17.795368,-63.1966639,"Cerca feria Barrio Lindo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7941421,-63.209281,"El trompillo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7457207,-63.1661523,"av Alemana entrando por calle 4"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7794582,-63.1987071,"Frente a cenetrop"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7683284,-63.2054836,"utepsa"));
+        ubicacionesLugar1.add(new Ubicacion(-17.795368,-63.1651211,"Rotonda de la avenida Pedro Marbán"));
+        ubicacionesLugar1.add(new Ubicacion(-17.755191023015392, -63.1721025454328,"Beni entre 3er y 4to anillo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.766583811916213, -63.19417544588414,"Av San Martin"));
+        ubicacionesLugar1.add(new Ubicacion(-17.77135863321891, -63.18733848610794,"2do anillo canal Isuto"));
+        ubicacionesLugar1.add(new Ubicacion(-17.77908772893547, -63.19410211065381,"Hernando Sanabria 2do anillo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.788931958212167, -63.18778971396487,"Av cañoto casi llegando a la ramada"));
+        ubicacionesLugar1.add(new Ubicacion(-17.798165648792388, -63.178970292333304,"El trompillo, Santos Dumont"));
+        ubicacionesLugar1.add(new Ubicacion(-17.768598167981636, -63.17094344227245,"Alemana Av Japon"));
+        ubicacionesLugar1.add(new Ubicacion(-17.7820151959431, -63.16350978458058,"Biopetrol virgen de cotoca"));
+        ubicacionesLugar1.add(new Ubicacion(-17.795161593583817, -63.16063257653148,"Tres pasos al frente, pedro marban"));
+        ubicacionesLugar1.add(new Ubicacion(-17.82336596934906, -63.16512933985187,"ave recolectora casi radial 12"));
+        ubicacionesLugar1.add(new Ubicacion(-17.785386584236104, -63.204178762036136,"Roca y coronado, roque aguilera"));
+        ubicacionesLugar1.add(new Ubicacion(-17.79278405829532, -63.20144867636005,"Av pirai, esq Moile"));
+        ubicacionesLugar1.add(new Ubicacion(-17.78967661150342, -63.1622907085801,"Terminal bimodal"));
+        ubicacionesLugar1.add(new Ubicacion(-17.71690785034076, -63.1625317929188,"Remanso 8vo anillo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.798188954916377, -63.17553249996259,"Madre India"));
+        ubicacionesLugar1.add(new Ubicacion(-17.774902658272538, -63.18232631535522,"1er anillo palacio de justicia"));
+        ubicacionesLugar1.add(new Ubicacion(-17.774855980683135, -63.174819815884014,"Av cañoto, justiniano"));
+        ubicacionesLugar1.add(new Ubicacion(-17.75823572393573, -63.14175246295036,"Parque Industrial"));
+        ubicacionesLugar1.add(new Ubicacion(-17.75591275380727, -63.16579671445474,"Alemana 3er anillo externo y 4to circunvalacion"));
+        ubicacionesLugar1.add(new Ubicacion(-17.80545090901727, -63.15488461059469,"Radial 10 4to anillo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.746643693951114, -63.17561038373896,"Las brisas"));
+        ubicacionesLugar1.add(new Ubicacion(-17.78319491404813, -63.18483134372722,"Sergio suarez figueroa"));
+        ubicacionesLugar1.add(new Ubicacion(-17.784900550220325, -63.17099001208038,"Av viedma, moldes"));
+        ubicacionesLugar1.add(new Ubicacion(-17.778229379942175, -63.14294393564685,"Virgen de cotoca, 4to anillo"));
+        ubicacionesLugar1.add(new Ubicacion(-17.774050470325665, -63.215816191556954,"4to anillo av centenario"));
+        ubicacionesLugar1.add(new Ubicacion(-17.761136064335663, -63.16260116425972,"3er anillo mutualista"));
+        ubicacionesLugar1.add(new Ubicacion(-17.797915144316903, -63.195560148908335,"grigota entre roque aguilera y trompillo"));
         mostrarMapa(ubicacionesLugar1);
     }
     public void mostrarMapaLugar2 (View view){ //banco ganadero
